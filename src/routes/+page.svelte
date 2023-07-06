b3e1c54fd8595cc87a5f8329433f03fbf452fa45
Ahmed <72154121+Ahmed-pk@users.noreply.github.com>
2023-07-06 20:55:37 +0500

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+
+let html = "";
+let css = "";
+
+function convert ()
+{
+
+}
+
+</script>
+
+
+<section class="py-16">
+    <div class="container mx-auto flex flex-col gap-6">
+        <h1 class="text-5xl text-center font-semibold">HTML2Tailwind</h1>
+        
+        <div class="flex justify-between gap-6">
+            <textarea class="border w-1/2 p-2 outline-none" bind:value={html} cols="30" rows="10" placeholder="Write HTML here"></textarea>
+            <textarea class="border w-1/2 p-2 outline-none" bind:value={css} cols="30" rows="10" placeholder="Write CSS here"></textarea>
+        </div>
+        <button class="px-6 py-3 bg-slate-600 text-white mx-auto text-xl rounded">Convert</button>
+    </div>
+</section>
