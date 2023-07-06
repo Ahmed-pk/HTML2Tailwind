ee8455bc1d9f47da94d3ed5118c6267fd1cf92ca
Ahmed <72154121+Ahmed-pk@users.noreply.github.com>
2023-07-06 22:02:21 +0500

    beta version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,27 @@
 <script>
+import { CssToTailwindTranslator } from "css-to-tailwind-translator";
 
 let html = "";
 let css = "";
 
+let output = "";
+
 function convert ()
 {
-    let parser = new CSSParser();
-    let sheet = parser.parse(css, false, true);
-    console.log(sheet);
+    let conversionResult = CssToTailwindTranslator(css, {useAllDefaultValues: true});
+    
+    let parser = new DOMParser();
+    let parsedHtml = parser.parseFromString(html, 'text/html');
+
+    
+    conversionResult.data.map( (element) => {
+        Array.from(parsedHtml.querySelectorAll(element.selectorName)).map(ele => {
+            ele.classList.add(element.resultVal);
+        });
+    });
+
+    output = parsedHtml.body.innerHTML;
+    
 }
 
 </script>
@@ -21,5 +35,10 @@ function convert ()
             <textarea class="border w-1/2 p-2 outline-none" bind:value={css} cols="30" rows="10" placeholder="Write CSS here"></textarea>
         </div>
         <button on:click={convert} class="px-6 py-3 bg-slate-600 text-white mx-auto text-xl rounded">Convert</button>
+
+        <h2 class="text-3xl font-semibold">Output:</h2>
+        <div class="border">
+            {output}
+        </div>
     </div>
 </section>
