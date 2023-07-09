acf87253c88e212bfee3f0a6047feceaede89210
Ahmed <72154121+Ahmed-pk@users.noreply.github.com>
2023-07-09 22:49:51 +0500

    changed UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,8 +35,8 @@ function convert ()
         <h1 class="text-5xl text-center font-semibold">HTML2Tailwind</h1>
         
         <div class="flex justify-between gap-6">
-            <textarea class="border w-1/2 p-2 outline-none" bind:value={html} cols="30" rows="10" placeholder="Write HTML here"></textarea>
             <textarea class="border w-1/2 p-2 outline-none" bind:value={css} cols="30" rows="10" placeholder="Write CSS here"></textarea>
+            <textarea class="border w-1/2 p-2 outline-none" bind:value={html} cols="30" rows="10" placeholder="Write HTML here"></textarea>
         </div>
         <button on:click={convert} class="px-6 py-3 bg-slate-600 text-white mx-auto text-xl rounded">Convert</button>
 
